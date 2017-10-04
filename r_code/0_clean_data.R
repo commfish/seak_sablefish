50e34dd25cb177c5afb1f6379e7c39d1a865a01c
Jane <ummjane@gmail.com>
2017-10-04 14:54:07 -0800

    change file name for survey cpue to be consistent with fishery data files. clean survey cpue.

--- r_code/0_clean_data.R
+++ r_code/0_clean_data.R
@@ -19,7 +19,7 @@ cpue <- cpue %>%
          Gear = factor(LONGLINE_SYSTEM_CODE),
          Hook_size = HOOK_SIZE,
          Size = factor(as.numeric(gsub("[^0-9]","",Hook_size))),
-         hook_space = HOOK_SPACING,
+         hook_space = HOOK_SPACING, #*FLAG*-check that this is in inches
          hooks_per_skate = HOOKS_PER_SKATE,
          Stat = factor(G_STAT_AREA),
          depth = AVERAGE_DEPTH_METERS,
@@ -38,10 +38,52 @@ cpue <- cpue %>%
          Size, hook_space, hooks_per_skate, Stat, depth, sets, no_hooks, 
          std_hooks, sable_wt_set, std_cpue)
 
-write.csv(cpue, "data/fishery/fishery_cpue_1997_2015.csv")
+write_rds(cpue, "data/fishery/fishery_cpue_1997_2015.rds")
 
-# Fishery harvest, catch time series----
+# Fishery harvest, catch time series ----
 
 # in progress...
 
-#
+# Survey cpue ----
+
+# ALEX QUERY CRITERIA FOR CPUE_SRV: *FLAG* where's depth? sets?
+# SURVEY >> LONGLINE SURVEY - CATCH AND HOOK ACCOUNTING
+# Base Table	out_g_sur_longline_hooks_catch																				
+# Select Clause	*																				
+#   Where Clause	project_code = '03' AND year IN (1988:present)																				
+# Group By Clause																					
+# Order By Clause	year, project_code, trip_no,effort_no,subset_no		
+
+srv_cpue <- read.csv("data/survey/raw_data/survey_cpue_1988_2016.csv")
+str(srv_cpue)
+srv_cpue <- srv_cpue %>% 
+  mutate(year = Year, #numeric
+         Year = as.factor(Year),
+         Stat = as.factor(G.Stat.Area),
+         station = Station.No,
+         subset_no = Subset.No, #*FLAG* no idea what this is
+         subset_condition = Subset.Condition #*FLAG* no idea what this is
+  ) %>%  
+  select(Year, year, trip_no = Trip.No, Project = Project.1, set = Set.No,
+         Stat, station, subset_condition, no_hooks = `Hooks...Total`, 
+         hooks_bait = `Hooks...Baited`, hooks_bare = `Hooks...Bare`, 
+         hooks_invalid = `Hooks...Invalid`, hooks_unknown = `Hooks...Uknown`,
+         no_sablefish = Sablefish, sable_per_hook = Sablefish.per.Hook
+  ) %>% 
+  filter(subset_condition == "Valid", #doesn't do anything
+         station < 100, # omit samping stations with 3 digits *FLAG* no documentation
+         !is.na(no_hooks) #doesn't do anything
+  ) %>% 
+  #standardize hook spacing (Sigler & Lunsford 2001, CJFAS)
+  #there was a change in hook spacing in 1997 from 2 to 3 m. *FLAG* check
+  mutate(std_hooks = ifelse(year > 1996, 2.2*no_hooks*(1-exp(-0.57*2)),
+                            2.2*no_hooks*(1-exp(-0.57*3))),
+         no_sablefish = ifelse(is.na(no_sablefish), 0, no_sablefish), # make any NAs 0 values
+         std_cpue = no_sablefish/std_hooks, #*FLAG* this is NPUE, the fishery is a WPUE
+         Station = as.factor(station)
+  )
+  
+write_rds(srv_cpue, "data/survey/survey_cpue_1988_2016.rds")
+
+# Survey biological ----
+srv_bio <- read.csv("data/survey/raw_data/survey_bio_1988_2016.csv")
