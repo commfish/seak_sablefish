1f455dce48b4cea2fbd5eb3f2907e91997d348c4
Ben Williams <ben.williams@alaska.gov>
2017-10-06 08:59:26 -0800

    just dropped a themeset as opposed to multiple themes

--- r_code/helper.R
+++ r_code/helper.R
@@ -10,62 +10,65 @@ if(!require("tidyverse"))   install.packages("tidyverse") # dplyr, ggplot, etc.
 if(!require("lubridate"))   install.packages("lubridate") # dates functions like yday, dmy
 if(!require("mgcv"))   install.packages("mgcv") # gams
 if(!require("gridExtra"))   install.packages("gridExtra") # multipanneled plots
-if(!require("magrittr"))   install.packages("magrittr") # piping
 
 # ggplot themes ----
 
-# basic eda (exporatory data analysis theme)
-eda_theme <- theme_classic() + 
-  theme(panel.grid.major = element_line(colour="grey95"),
-        panel.grid.minor = element_line(colour="grey95"),
-        strip.text = element_text(size=10),
-        axis.text.x = element_text(size=10, angle=45, hjust=1),
-        axis.text.y = element_text(size=10),
-        axis.title.x = element_text(size=10),
-        axis.title.y = element_text(size=10),
-        legend.background = element_rect(fill = "transparent", colour = NA))
+theme_set(theme_bw(base_size=12)+ 
+            theme(panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank()))
 
-# eda theme for faceted figs
-eda_facet <- theme_bw() + 
-  theme(strip.text = element_text(size=10),
-        axis.text.x = element_text(size=10, colour="black", angle=45, hjust=1),
-        axis.text.y = element_text(size=10, colour="black"),
-        axis.title.x = element_text(size=10, colour="black"),
-        axis.title.y = element_text(size=10, colour="black"),
-        plot.background = element_rect(fill = "transparent", colour = NA),
-        legend.background = element_rect(fill = "transparent", colour = NA)) 
-
-# mapping theme
-map_theme <- theme(
-  plot.title = element_text(size = rel(1.5)),
-  axis.line=element_blank(),
-  axis.text.x=element_blank(),
-  axis.text.y=element_blank(),
-  axis.ticks=element_blank(),
-  axis.title.x=element_blank(),
-  axis.title.y=element_blank(),
-  #legend.position="bottom",
-  panel.background=element_blank(),
-  panel.border=element_blank(),
-  panel.grid.major=element_blank(),
-  panel.grid.minor=element_blank(),
-  #legend.text=element_text(size = rel(1.5)),
-  plot.background=element_blank())
-
-# barplot theme
-bar_theme <- theme_bw() + 
-  theme(axis.line = element_line(colour = "black"),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        axis.text.x=element_text(angle = 45, hjust = 1), #angle = 45
-        #axis.title.y=element_text(angle=0),
-        panel.background = element_blank())
-
-# closer to publication-style theme
-report_theme <- theme_bw() +
-  theme(text = element_text(size = 12),
-        axis.title.y = element_text(size = 12),
-        axis.title.x = element_text(size = 12),
-        strip.text.x = element_text(size = 14),
-        axis.line = element_line(size = 0.5, color = "black"),
-        legend.position = "bottom")
+# # basic eda (exporatory data analysis theme)
+# eda_theme <- theme_classic() + 
+#   theme(panel.grid.major = element_line(colour="grey95"),
+#         panel.grid.minor = element_line(colour="grey95"),
+#         strip.text = element_text(size=10),
+#         axis.text.x = element_text(size=10, angle=45, hjust=1),
+#         axis.text.y = element_text(size=10),
+#         axis.title.x = element_text(size=10),
+#         axis.title.y = element_text(size=10),
+#         legend.background = element_rect(fill = "transparent", colour = NA))
+# 
+# # eda theme for faceted figs
+# eda_facet <- theme_bw() + 
+#   theme(strip.text = element_text(size=10),
+#         axis.text.x = element_text(size=10, colour="black", angle=45, hjust=1),
+#         axis.text.y = element_text(size=10, colour="black"),
+#         axis.title.x = element_text(size=10, colour="black"),
+#         axis.title.y = element_text(size=10, colour="black"),
+#         plot.background = element_rect(fill = "transparent", colour = NA),
+#         legend.background = element_rect(fill = "transparent", colour = NA)) 
+# 
+# # mapping theme
+# map_theme <- theme(
+#   plot.title = element_text(size = rel(1.5)),
+#   axis.line=element_blank(),
+#   axis.text.x=element_blank(),
+#   axis.text.y=element_blank(),
+#   axis.ticks=element_blank(),
+#   axis.title.x=element_blank(),
+#   axis.title.y=element_blank(),
+#   #legend.position="bottom",
+#   panel.background=element_blank(),
+#   panel.border=element_blank(),
+#   panel.grid.major=element_blank(),
+#   panel.grid.minor=element_blank(),
+#   #legend.text=element_text(size = rel(1.5)),
+#   plot.background=element_blank())
+# 
+# # barplot theme
+# bar_theme <- theme_bw() + 
+#   theme(axis.line = element_line(colour = "black"),
+#         panel.grid.major = element_blank(),
+#         panel.grid.minor = element_blank(),
+#         axis.text.x=element_text(angle = 45, hjust = 1), #angle = 45
+#         #axis.title.y=element_text(angle=0),
+#         panel.background = element_blank())
+# 
+# # closer to publication-style theme
+# report_theme <- theme_bw() +
+#   theme(text = element_text(size = 12),
+#         axis.title.y = element_text(size = 12),
+#         axis.title.x = element_text(size = 12),
+#         strip.text.x = element_text(size = 14),
+#         axis.line = element_line(size = 0.5, color = "black"),
+#         legend.position = "bottom")
