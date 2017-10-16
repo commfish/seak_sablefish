2281cc73e6f562621f2ab971ac21f06fbaaf6295
Jane <ummjane@gmail.com>
2017-10-16 12:48:56 -0800

    finished growth analysis and comparison with nmfs values.

--- r_code/biological.R
+++ r_code/biological.R
@@ -3,7 +3,6 @@
 # Contact: jane.sullivan1@alaska.gov
 # Last edited: 2017-10-09
 
-# load ----
 source("r_code/helper.R")
 library("broom")
 library("stats4")
@@ -29,123 +28,38 @@ srv_bio %>%
 # Hanselman et al. 2007 Appendix GOA Sablefish SAFE Appendix 3C for comparison.
 # The coastwide parameters are still used for management, but Southeast
 # slope are informative.
+noaa_lvb <- read_csv("data/survey/noaa_lvb_params_hanselman2007.csv")
 
-nmfs_laa_lvb <- data.frame(Sex = c(rep("Female", 2),
-                                   rep("Male", 2)),
-                           Survey = c(rep(c("NMFS LL Survey",
-                                            "NMFS LL Survey"), 
-                                          2)),
-                           Region = c(rep(c("Southeast slope",
-                                            "GOA/BS/AI coastwide"), 
-                                          2)),
-                           Years = rep("1996 - 2004", 4),
-                           Linf = c(80.22, 80.92, 67.77, 68.34),
-                           Linf_se = c(0.22, NA, 0.13, NA),
-                           k = c(0.22, 0.27, 0.29, 0.31),
-                           k_se = c(0.01, NA, 0.01, NA),
-                           t0 = c(-1.95, -0.85, -2.27, -1.71),
-                           t0_se = c(0.12, NA, 0.17, NA),
-                           n = c(5767, 515, 4889, 605))
+glimpse(noaa_lvb)
 
-nmfs_waa_allom <- data.frame(Sex = c("Female", "Male"),
-                             Survey = rep("NMFS LL Survey", 2),
-                             Region = rep("GOA/BS/AI coastwide", 2),
-                             Years = rep("1996 - 2004", 2),
-                             alpha = c(1.01e-05, 1.24e-05),
-                             alpha_se = c(NA, NA),
-                             beta = c(3.015, 2.960),
-                             beta_se = c(NA, NA),
-                             n = c(5767, 4889))
+# Length-based Ludwig von Bertalanffy growth model -----
 
-nmfs_waa_lvb <- data.frame(Sex = c("Female", "Male"),
-                           Survey = rep("NMFS LL Survey", 2),
-                           Region = rep("GOA/BS/AI coastwide", 2),
-                           Years = rep("1996 - 2004", 2),
-                           Winf = c(5.47, 3.16),
-                           Winf_se = c(NA, NA),
-                           k = c(0.24, 0.36),
-                           k_se = c(NA, NA), 
-                           t0 = c(-1.39, -1.13),
-                           t0_se = c(NA, NA))
-
-# length-at-age using Ludwig von Bertalanffy growth model -----
-
-lvb_laa <- function(t, l_inf, k, t0) {
-  l_inf * (1 - exp(-k * (t - t0)) )
-}
-
+# subsets by length, age, sex
 srv_bio %>% 
   filter(Sex %in% c("Female", "Male") &
-           year >= 1997 & #advent of "modern" survey
+           year >= 1997 & # *FLAG* advent of "modern" survey
            !is.na(length_cm) &
            !is.na(age)) %>% 
   droplevels() -> laa_sub
 
-#sex-specific starting values close to Hanselman et al. 2007 (Appendix C, Table 1)
-START_f <- c(l_inf = 80, k = 0.22, t0 = -1.9) 
-START_m <- c(l_inf = 68, k = 0.29, t0 = -2.3)
-
-fem_fit <- nls(length_cm ~ lvb_laa(t = age, l_inf, k, t0), 
-               data = filter(laa_sub, Sex == "Female"), 
-               trace = TRUE, algorithm = "port", 
-               lower = c(60, 0.02, -5), upper = c(100, 0.5, 5),
-               start = START_f)
-# *FLAG* nls() having problems estimating parameters, hitting lower bounds of
-# t0. next steps: optim(), admb
-
-# multiplicative error structure (increasing variance with age)
-fem_fit_log <- nls(log(length_cm) ~ log(lvb_laa(t = age, l_inf, k, t0)), 
-               data = filter(laa_sub, Sex == "Female"), 
-               trace = TRUE, algorithm = "port", 
-               lower = c(60, 0.02, -5), upper = c(100, 0.5, 5),
-               start = START_f)
-
-male_fit <- nls(length_cm ~ lvb_laa(t = age, l_inf, k, t0), 
-                data = filter(laa_sub, Sex == "Male"), 
-                trace = TRUE, algorithm = "port", 
-                lower = c(30, 0.01, -5), upper = c(80, 0.5, 5), start = START_m)
-
-# multiplicative error structure (increasing variance with age)
-male_fit_log <- nls(log(length_cm) ~ log(lvb_laa(t = age, l_inf, k, t0)), 
-                data = filter(laa_sub, Sex == "Male"), 
-                trace = TRUE, algorithm = "port", 
-                lower = c(30, 0.01, -5), upper = c(80, 0.5, 5), start = START_m)
-
-#plot
-ggplot(laa_sub,  
-       aes(x = age, y = length_cm, col = Sex, shape = Sex)) +
-  geom_jitter() + #alpha = .8
-  stat_function(fun = lvb_laa, 
-                args = as.list(tidy(fem_fit)$estimate),
-                lwd = 2, col = "salmon") + #"salmon"
-  stat_function(fun = lvb_laa, 
-                args = as.list(tidy(fem_fit_log)$estimate),
-                lwd = 2, col = "pink") + #"salmon"
-  stat_function(fun = lvb_laa, 
-                args = as.list(tidy(male_fit)$estimate),
-                lwd = 2, col = "blue") + #"#00BFC4"
-  stat_function(fun = lvb_laa, 
-                args = as.list(tidy(male_fit_log)$estimate),
-                lwd = 2, col = "lightblue") + #"#00BFC4"
-  scale_color_manual(values = c("salmon", "blue")) +
-  xlab("\nAge (yrs)") +
-  ylab("Length (cm)\n")
+laa_sub %>% ungroup() %>% filter(Sex == "Female") -> laa_f
+laa_sub %>% ungroup() %>% filter(Sex == "Male") -> laa_m
 
 # fit length-based lvb with max likelihood 
 vb_like <- function(obs_length, age, l_inf, k, t0, sigma) { 
-  pred <- l_inf * (1 - exp(-k * (age - t0))) # predictions based on von bertalanffy growth curve
+  pred <- l_inf * (1 - exp(-k * (age - t0))) # predictions based on lvb growth curve
   like <- dnorm(obs_length, pred, sigma) # likelihood
   neg_like <- -1 * (sum(log(like))) # negative log likelihood
   return(neg_like) # returning negative log likelihood
 }
 
-vb_mle <- function(obs_length, age, starting_vals) {
+vb_mle <- function(obs_length, age, starting_vals, sex) {
   #minimizing negative log likelihood with mle function
   vb_mle <- mle(vb_like, start = as.list(starting_vals), 
              fixed = list(obs_length = obs_length, age = age),
              method = "BFGS")
   print(summary(vb_mle))
-  l_inf_opt <- coef(summary(vb_mle))[1] #retaining optimized parameter values
+  l_inf_opt <- coef(summary(vb_mle))[1] # parameter estimates
   k_opt <- coef(summary(vb_mle))[2]
   t0_opt <- coef(summary(vb_mle))[3]
   sigma_opt <- coef(summary(vb_mle))[4]
@@ -153,41 +67,55 @@ vb_mle <- function(obs_length, age, starting_vals) {
   pred <- l_inf_opt * (1 - exp(-k_opt * (age - t0_opt))) # retaining predicted values
   resids <- obs_length - pred # retaining residuals
   results <- list(predictions = data.frame(obs_length = obs_length,
-                                           age = age, pred = pred, resid = resids),
-                  results = coef(summary(vb_mle)), logl = logl)
+                                           age = age, pred = pred, 
+                                           resid = resids, Sex = sex),
+                  results = tidy(coef(summary(vb_mle))) %>% 
+                    select(Parameter = `.rownames`, Estimate, SE = `Std..Error`) %>% 
+                    mutate(Sex = sex), 
+                  logl = logl)
   return(results)
 }
 
-laa_sub %>% ungroup() %>% filter(Sex == "Female") -> laa_f
-laa_sub %>% ungroup() %>% filter(Sex == "Male") -> laa_m
-
-#sex-specific starting values close to Hanselman et al. 2007 (Appendix C, Table 1)
+#sex-specific starting values from Hanselman et al. 2007 (Appendix C, Table 1),
+#except sigma
 start_f <- c(l_inf = 80, k = 0.22, t0 = -1.9, sigma = 10) 
 start_m <- c(l_inf = 68, k = 0.29, t0 = -2.3, sigma = 10)
 
 # mle fit for females
 vb_mle_f <- vb_mle(obs_length = laa_f$length_cm,
                    age = laa_f$age,
-                   starting_vals = start_f)
-
-pred_f <- vb_mle_f$predictions %>% mutate(Sex = "Female")
+                   starting_vals = start_f,
+                   sex = "Female")
 
 # mle fit for males
 vb_mle_m <- vb_mle(obs_length = laa_m$length_cm,
                    age = laa_m$age,
-                   starting_vals = start_m)
-pred_m <- vb_mle_m$predictions %>% mutate(Sex = "Male")
+                   starting_vals = start_m,
+                   sex = "Male")
 
-# combine output and plot mle
-pred <- rbind(pred_f, pred_m) %>% 
+# combine predictions and parameter estimates and plot mle
+pred <- rbind(vb_mle_f$predictions, vb_mle_m$predictions) %>% 
   mutate(std_resid = resid/sd(resid))
 
+
+lvb_pars <- full_join(
+  rbind(vb_mle_f$results, vb_mle_m$results) %>% 
+  mutate(Survey = "ADF&G Longline",
+         Years = paste0(min(laa_sub$year), "-", max(laa_sub$year)),
+         Region = "Chatham Strait",
+         Function = "Length-based LVB")
+  ,
+  laa_sub %>% group_by(Sex) %>% summarise(n = n()),
+  by = "Sex")
+
+png("figures/length_vonb_chatham_1997_2016.png", height = 4, width = 6, units = "in", res = 300)
 ggplot() +
   geom_jitter(data = laa_sub, aes(x = age, y = length_cm, col = Sex, shape = Sex)) +
   geom_line(data = pred, aes(x = age, y = pred, col = Sex, group = Sex), lwd = 2 ) + #"#00BFC4"
   geom_line(data = pred, aes(x = age, y = pred, group = Sex), col = "black" ) + #"#00BFC4"
   xlab("\nAge (yrs)") +
   ylab("Length (cm)\n")
+dev.off()
 
 # residual plots
 ggplot(data = pred) + 
@@ -210,16 +138,18 @@ ggplot(data = pred) +
 # al. 2007 values beta_f = 3.02 and beta_m = 2.96 are used in the current
 # assessment)
 
-lw_allometry <- function(length, a, b) {
-  a * length ^ b
-}
 
+# subsets by weight, age, sex
 srv_bio %>% 
   filter(Sex %in% c("Female", "Male") &
            year >= 1997 & #advent of "modern" survey
            !is.na(length_cm) &
-           !is.na(weight_kg)) %>% 
-  droplevels() -> allom_sub
+           !is.na(weight_kg)) %>% droplevels() -> allom_sub
+
+# length-weight relationship
+lw_allometry <- function(length, a, b) {
+  a * length ^ b
+}
 
 START <- c(a = 1e-5, b = 3) #Starting values close to Hanselman et al. 2007
 
@@ -229,71 +159,49 @@ fem_fit <- nls(weight_kg ~ lw_allometry(length = length_cm, a, b),
 male_fit <- nls(weight_kg ~ lw_allometry(length = length_cm, a, b), 
                data = filter(allom_sub, Sex == "Male"), start = START)
 
-ggplot(allom_sub,  
-       aes(x = length_cm, y = weight_kg, col = Sex, shape = Sex)) +
-  geom_jitter() + #alpha = .8
-  stat_function(fun = lw_allometry, 
-                args = as.list(tidy(fem_fit)$estimate),
-                lwd = 1.5, col = "salmon") + #"salmon"
-  stat_function(fun = lw_allometry, 
-                args = as.list(tidy(male_fit)$estimate),
-                lwd = 1.5, col = "blue", lty = 2) + #"#00BFC4"
-  scale_color_manual(values = c("salmon", "blue")) +
-  xlab("\nLength (cm)") +
-  ylab("Weight (kg)\n")
-  
+# parameter estimates and plot fit 
 beta_m <- tidy(male_fit)$estimate[2]
 beta_f <- tidy(fem_fit)$estimate[2]
 
+allom_pars <- full_join(
+  rbind(tidy(male_fit) %>% mutate(Sex = "Male"),
+        tidy(fem_fit) %>% mutate(Sex = "Female")
+  ) %>% 
+    select(Parameter = term, Estimate = estimate, SE = std.error, Sex) %>% 
+    mutate(Survey = "ADF&G Longline",
+           Years = paste0(min(laa_sub$year), "-", max(laa_sub$year)),
+           Region = "Chatham Strait",
+           Function = "Allometric")
+  ,
+  allom_sub %>% group_by(Sex) %>% summarise(n = n()),
+  by = "Sex")
+
+png("figures/allometry_chatham_1997_2016.png", height = 4, width = 6, units = "in", res = 300)
+ggplot(allom_sub,  
+       aes(x = length_cm, y = weight_kg, col = Sex, shape = Sex)) +
+  geom_jitter() + 
+  stat_function(fun = lw_allometry, 
+                args = as.list(tidy(fem_fit)$estimate),
+                lwd = 1.5, col = "salmon") + 
+  stat_function(fun = lw_allometry, 
+                args = as.list(tidy(male_fit)$estimate),
+                lwd = 1.5, col = "#00BFC4", lty = 2) + 
+  xlab("\nLength (cm)") +
+  ylab("Weight (kg)\n")
+dev.off()
+  
 # weight-at-age using Ludwig von Bertalanffy growth model ----
-# use sex-spec allometric betas. probably going to need to use a
-# multiplicative error structure
 
-lvb_waa <- function(weight, t, k, w_inf, t0, b) {
-  pred <- w_inf * (1 - exp(-k * (t - t0) ) ) ^ b
-  pred <- ifelse(pred < 0, abs(pred), pred)
-  log(weight) - log(pred)
-}
-
+# subsets by weight, age, sex
 srv_bio %>% 
   filter(Sex %in% c("Female", "Male") &
-           year >= 1997 & #advent of "modern" survey
+           year >= 1997 & # *FLAG* advent of "modern" survey
            !is.na(age) &
            !is.na(weight_kg)) %>% 
   droplevels() -> waa_sub
 
-# starting values from Hanselman et al. 2007 Appendix C Table 5
-start_f <- c(w_inf = 5.5, k = 0.24, t0 = -1.4, b = beta_f)
-start_m <- c(w_inf = 3.2, k = 0.36, t0 = -1.1, b = beta_m)
-
-fem_waa <- nls(weight_kg ~ lvb_waa(weight = weight_kg, t = age, 
-                                   w_inf, k, t0, b = beta_f), 
-               data = filter(allom_sub, Sex == "Female"), 
-               start = START_f)
-
-male_waa <- nls(weight_kg ~ lvb_waa(weight = weight_kg, t = age, 
-                                   w_inf, k, t0, b = beta_m), 
-               data = filter(allom_sub, Sex == "Male"), 
-               start = START_m)
-
-ggplot(waa_sub,  
-       aes(x = age, y = weight_kg, col = Sex, shape = Sex)) +
-  geom_jitter() + #alpha = .8
-  stat_function(fun = lvb_waa, 
-                args = START_f,
-                lwd = 2, col = "salmon") + #"salmon"
-  # stat_function(fun = lvb_laa, 
-  #               args = as.list(tidy(fem_fit_log)$estimate),
-  #               lwd = 2, col = "pink") + #"salmon"
-  stat_function(fun = lvb_waa, 
-                args = START_m,
-                lwd = 2, col = "blue") + #"#00BFC4"
-  # stat_function(fun = lvb_laa, 
-  #               args = as.list(tidy(male_fit_log)$estimate),
-  #               lwd = 2, col = "lightblue") + #"#00BFC4"
-  scale_color_manual(values = c("salmon", "blue")) +
-  xlab("\nAge (yrs)") +
-  ylab("Weight (kg)\n")
+waa_sub %>% ungroup() %>% filter(Sex == "Female") -> waa_f
+waa_sub %>% ungroup() %>% filter(Sex == "Male") -> waa_m
 
 # fit weight-based lvb with a multiplicative error structure using max likelihood estimation
 # log(w_i) = log(w_inf) + beta * log(1 - exp * (-k * (age_i - t0))) + error
@@ -306,7 +214,7 @@ wvb_like <- function(obs_weight, age, w_inf, k, t0, b, sigma) {
   return(neg_like) # returning negative log likelihood
 }
 
-wvb_mle <- function(obs_weight, age, b, starting_vals) {
+wvb_mle <- function(obs_weight, age, b, starting_vals, sex) {
   #minimizing negative log likelihood with mle function
   wvb_mle <- mle(wvb_like, start = as.list(starting_vals), 
                 fixed = list(obs_weight = obs_weight, age = age, b = b),
@@ -321,14 +229,14 @@ wvb_mle <- function(obs_weight, age, b, starting_vals) {
   pred <- exp(log_pred)
   resids <- obs_weight - pred # retaining residuals
   results <- list(predictions = data.frame(obs_weight = obs_weight,
-                                           age = age, pred = pred, resid = resids),
-                  results = coef(summary(wvb_mle)), logl = logl)
+                            age = age, pred = pred, 
+                            resid = resids, Sex = sex),
+    results = tidy(coef(summary(wvb_mle))) %>% 
+      select(Parameter = `.rownames`, Estimate, SE = `Std..Error`) %>% 
+      mutate(Sex = sex), 
+    logl = logl)
   return(results)
 }
-
-
-waa_sub %>% ungroup() %>% filter(Sex == "Female") -> waa_f
-waa_sub %>% ungroup() %>% filter(Sex == "Male") -> waa_m
 
 # starting values from Hanselman et al. 2007 Appendix C Table 5
 start_f <- c(w_inf = 5.5, k = 0.24, t0 = -1.4, sigma = 10)
@@ -338,27 +246,40 @@ start_m <- c(w_inf = 3.2, k = 0.36, t0 = -1.1, sigma = 10)
 wvb_mle_f <- wvb_mle(obs_weight = waa_f$weight_kg,
                    age = waa_f$age,
                    b = beta_f,
-                   starting_vals = start_f)
-
-wpred_f <- wvb_mle_f$predictions %>% mutate(Sex = "Female")
+                   starting_vals = start_f,
+                   sex = "Female")
 
 # mle fit for males
 wvb_mle_m <- wvb_mle(obs_weight = waa_m$weight_kg,
                    age = waa_m$age,
                    b = beta_m, 
-                   starting_vals = start_m)
-wpred_m <- wvb_mle_m$predictions %>% mutate(Sex = "Male")
+                   starting_vals = start_m,
+                   sex = "Male")
 
-# combine output and plot mle
-pred <- rbind(wpred_f, wpred_m) %>% 
+
+# combine predictions and parameter estimates and plot fitted values
+pred <- rbind(wvb_mle_f$predictions, wvb_mle_m$predictions) %>% 
   mutate(std_resid = resid/sd(resid))
 
+
+wvb_pars <- full_join(
+  rbind(wvb_mle_f$results, wvb_mle_m$results) %>% 
+    mutate(Survey = "ADF&G Longline",
+           Years = paste0(min(waa_sub$year), "-", max(waa_sub$year)),
+           Region = "Chatham Strait",
+           Function = "Weight-based LVB")
+  ,
+  waa_sub %>% group_by(Sex) %>% summarise(n = n()),
+  by = "Sex")
+
+png("figures/weight_vonb_chatham_1997_2016.png", height = 4, width = 6, units = "in", res = 300)
 ggplot() +
   geom_jitter(data = waa_sub, aes(x = age, y = weight_kg, col = Sex, shape = Sex)) +
   geom_line(data = pred, aes(x = age, y = pred, col = Sex, group = Sex), lwd = 2 ) + #"#00BFC4"
   geom_line(data = pred, aes(x = age, y = pred, group = Sex), col = "black" ) + #"#00BFC4"
   xlab("\nAge (yrs)") +
   ylab("Weight (kg))\n")
+dev.off()
 
 # residual plots
 ggplot(data = pred) + 
@@ -375,7 +296,16 @@ ggplot(data = pred) +
   geom_hline(aes(yintercept = 0), linetype = 2, col = "red") + 
   facet_wrap(~ Sex)
 
+# Compare growth results ----
+
 # Comparison of Hanselman et al. 2007 values with the Chatham Strait longline
 # survey. Units: length (cm), weight (kg), and age (yrs)
 
-laa_hanselman <- data.frame()
+rbind(allom_pars,
+      rbind(lvb_pars, wvb_pars)) %>%
+      mutate(Source = "seak_sablefish/code/biological.r") -> adfg_lvb 
+      
+write_csv(rbind(noaa_lvb, adfg_lvb) , 
+          "output/compare_vonb_adfg_noaa.csv")
+
+# 
