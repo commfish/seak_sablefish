d292b314420a316dcae2466c8cf9807d7907409b
Jane <ummjane@gmail.com>
2018-05-11 09:29:22 -0800

    change to theme_sleek and FNGr.

--- r/helper.R
+++ r/helper.R
@@ -19,72 +19,33 @@ if(!require("padr"))   install.packages("padr") # fills in missing values in a t
 if(!require("tidyr"))   install.packages("tidyr") # reshaping data
 if(!require("knitr"))   install.packages("knitr") # r markdown
 if(!require("forcats"))   install.packages("forcats") # releveling factors
+# install.packages("devtools")
+# devtools::install_github("ben-williams/FNGr")
+library("FNGr")
 
 # ggplot themes ----
 
-theme_set(theme_bw(base_size=12)+ 
-            theme(panel.grid.major = element_blank(),
-                  panel.grid.minor = element_blank(),
-                  axis.text.x = element_text(size = 14), # angle = 45, hjust = 1),
-                  axis.text.y = element_text(size = 14),
-                  axis.title.x = element_text(size = 14),
-                  axis.title.y = element_text(size = 14),
-                  strip.text.x = element_text(size = 14),
-                  strip.text.y = element_text(size = 14),
-                  legend.key=element_blank()))
+theme_sleek <- function(base_size = 12, base_family = "") {
+  half_line <- base_size/2
+  theme_light(base_size = 12, base_family = "") +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      axis.ticks.length = unit(half_line / 2.2, "pt"),
+      strip.background = element_rect(fill = NA, colour = NA),
+      strip.text.x = element_text(colour = "grey30"),
+      strip.text.y = element_text(colour = "grey30"),
+      axis.text = element_text(colour = "grey30"),
+      axis.title = element_text(colour = "grey30"),
+      legend.title = element_text(colour = "grey30", size = rel(0.9)),
+      panel.border = element_rect(fill = NA, colour = "grey70", size = 1),
+      legend.key.size = unit(0.9, "lines"),
+      legend.text = element_text(size = rel(0.7), colour = "grey30"),
+      legend.key = element_rect(colour = NA, fill = NA),
+      legend.background = element_rect(colour = NA, fill = NA),
+      plot.title = element_text(colour = "grey30", size = rel(1)),
+      plot.subtitle = element_text(colour = "grey30", size = rel(.85))
+    )
+}
 
-# # basic eda (exporatory data analysis theme)
-# eda_theme <- theme_classic() +
-#   theme(panel.grid.major = element_line(colour="grey95"),
-#         panel.grid.minor = element_line(colour="grey95"),
-#         strip.text = element_text(size=10),
-        # axis.text.x = element_text(size=10, angle=45, hjust=1),
-        # axis.text.y = element_text(size=10),
-        # axis.title.x = element_text(size=10),
-        # axis.title.y = element_text(size=10),
-#         legend.background = element_rect(fill = "transparent", colour = NA))
-
-# # eda theme for faceted figs
-# eda_facet <- theme_bw() + 
-#   theme(strip.text = element_text(size=10),
-#         axis.text.x = element_text(size=10, colour="black", angle=45, hjust=1),
-#         axis.text.y = element_text(size=10, colour="black"),
-#         axis.title.x = element_text(size=10, colour="black"),
-#         axis.title.y = element_text(size=10, colour="black"),
-#         plot.background = element_rect(fill = "transparent", colour = NA),
-#         legend.background = element_rect(fill = "transparent", colour = NA)) 
-# 
-# # mapping theme
-# map_theme <- theme(
-#   plot.title = element_text(size = rel(1.5)),
-#   axis.line=element_blank(),
-#   axis.text.x=element_blank(),
-#   axis.text.y=element_blank(),
-#   axis.ticks=element_blank(),
-#   axis.title.x=element_blank(),
-#   axis.title.y=element_blank(),
-#   #legend.position="bottom",
-#   panel.background=element_blank(),
-#   panel.border=element_blank(),
-#   panel.grid.major=element_blank(),
-#   panel.grid.minor=element_blank(),
-#   #legend.text=element_text(size = rel(1.5)),
-#   plot.background=element_blank())
-# 
-# # barplot theme
-# bar_theme <- theme_bw() + 
-#   theme(axis.line = element_line(colour = "black"),
-#         panel.grid.major = element_blank(),
-#         panel.grid.minor = element_blank(),
-#         axis.text.x=element_text(angle = 45, hjust = 1), #angle = 45
-#         #axis.title.y=element_text(angle=0),
-#         panel.background = element_blank())
-# 
-# # closer to publication-style theme
-# report_theme <- theme_bw() +
-#   theme(text = element_text(size = 12),
-#         axis.title.y = element_text(size = 12),
-#         axis.title.x = element_text(size = 12),
-#         strip.text.x = element_text(size = 14),
-#         axis.line = element_line(size = 0.5, color = "black"),
-#         legend.position = "bottom")
+theme_set(theme_sleek())
