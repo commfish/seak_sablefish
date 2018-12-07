1817db1a08867ba4fe51e0bc0dbb25b58fca264d
Sullivan <ummjane@gmail.com>
2018-12-07 06:09:14 -0900

    update gitignore

---
+++ tmb/mod2/mod1.cpp
@@ -0,0 +1,155 @@
+#include <TMB.hpp>
+
+template <class Type> Type square(Type x){return x*x;}
+
+template<class Type>
+  Type objective_function<Type>::operator() ()
+{
+  // DATA SECTION:
+    
+  // Model dimensions
+  DATA_INTEGER(nyr)          // number of years in the model
+  DATA_INTEGER(nage)         // number of ages in the model
+  DATA_INTEGER(cm_comp_nyr)  // number of years containing age comp data
+  DATA_IVECTOR(cm_comp_yrs)   // year with age comp data
+  
+  // Fixed parameters
+  DATA_SCALAR(M)             // fixed natural mortality
+  DATA_SCALAR(sig_catch)     // assumed CV for catch
+  DATA_SCALAR(sig_cm_cpue)   // assumed CV for log CPUE
+  DATA_SCALAR(omega_comp)    // effective sample size for age comps //assumed variance (?) for age comps
+  
+  // Data inputs
+  DATA_VECTOR(data_cm_waa)   // commercial weight-at-age (constant across years)
+  DATA_VECTOR(data_catch)    // catch in year i
+  DATA_VECTOR(data_cm_cpue)  // commercial cpue in year i
+  DATA_MATRIX(data_cm_comp)  // commercial age compositions in year i
+  
+  // PARAMETER SECTION:
+  PARAMETER(dummy);        // dummy variable for troubleshooting 
+  PARAMETER_VECTOR(logN);    // log abundance in year i
+  PARAMETER(cm_sel50);       // commercial selectivity parameter (50% selected)
+  PARAMETER(cm_sel95);       // commercial selectivity parameter (95% selected)
+  PARAMETER_VECTOR(logF);    // log full fishing mortality in year i
+  PARAMETER(logq);           // catchability coefficient
+  
+  matrix<Type> Nij(nyr+1,nage);     // Abundance in year i, age j
+  matrix<Type> Fij(nyr,nage);       // Fishing mortality in year i, age j         
+  matrix<Type> Zij(nyr,nage);       // Total mortality in year i, age
+  vector<Type> Sj(nage);            // Commercial selectivity at age j
+  vector<Type> Fi(nyr);             // Full fishing mortality in year i
+  
+  vector<Type> pred_catch(nyr);         // Predicted catch
+  vector<Type> pred_cm_cpue(nyr);       // Predicted commercial cpue
+  matrix<Type> pred_cm_comp(nyr,nage);  // Predicted commercial age comps
+  vector<Type> Bi(nyr);                 // Biomass  
+  
+  Type Like1;
+  Type Like2;
+  Type Like3;
+  Type obj_fun;
+  
+  // End of specifications section
+  // =============================
+    
+    // Specify selectivity
+  for (int j=0;j<nage;j++)
+    Sj(j) = 1.0/(1+exp(-log(19)*(j-cm_sel50)/(cm_sel95-cm_sel50)));
+  
+  // Compute the F and Z matrices
+  for (int i=0;i<nyr;i++)
+    for (int j=0;j<nage;j++)
+    {
+      Fi(i) = exp(logF(i));
+      Fij(i,j) = exp(logF(i))*Sj(j);
+      Zij(i,j) = M + Fij(i,j);
+    }
+  
+  // Insert all the recritments (watch out for the index pointers)
+  for (int j=0;j<nage;j++) Nij(0,j) = exp(logN(nage-j-1));
+  for (int i=1;i<nyr;i++) Nij(i,0) = exp(logN(nage+i-1)); //mean recruitment multiplied by log deviation
+  Nij(nyr,0) = 0;
+  
+  // Project the whole N-matrix
+  for (int i=0;i<nyr;i++)
+    for (int j=0;j<nage-1;j++)
+      Nij(i+1,j+1) = Nij(i,j)*exp(-Zij(i,j));
+  
+  // Compute the predicted exploitable biomass, catch-at-age and catch
+  Type Cij; // catch at age j in numbers in year j (temporary variable)
+  for (int i=0;i<nyr;i++)
+  {
+    Bi(i) = 0; pred_catch(i) = 0; Cij = 0;
+    for (int j=0;j<nage;j++)
+    {
+      // Multiple by 100 to get N and catch on the same scale
+      pred_cm_comp(i,j) = Fij(i,j)/Zij(i,j)*Nij(i,j)*(1.0-exp(-Zij(i,j)))*100;
+      pred_catch(i) += data_cm_waa(j)*pred_cm_comp(i,j);
+      Bi(i) += data_cm_waa(j)*Sj(j)*Nij(i,j)*exp(-Zij(i,j)/2.0);
+      Cij += pred_cm_comp(i,j);
+    }
+    pred_cm_cpue(i) = exp(logq)*Bi(i);
+    for (int j=0;j<nage;j++) pred_cm_comp(i,j) /= Cij;
+  }
+  
+  // Likelihood components
+  // 0: Catch
+  // 1: Commercial cpue
+  // 2: Commercial age comps
+  
+  // vector<Type> nll(3);
+  // nll.setZero();
+  
+  // Catch data (normal likelihood)
+  Like1 = 0;  
+  for (int i=0;i<nyr;i++)
+    Like1 += square((data_catch(i)-pred_catch(i))/pred_catch(i));
+  Like1 = Like1 / (2.0*square(sig_catch));
+  
+  // CPUE data (lognormal likelihood)
+  Like2 = 0;
+  for (int i=0;i<nyr;i++)
+    Like1 += square(log(data_cm_cpue(i)) - log(pred_cm_cpue(i)));
+  Like2 = Like2 / (2.0*square(sig_cm_cpue));
+  
+  // Catch-at-age data (multinomial likelihood)
+  Like3 = 0;
+  
+  matrix<Type> tmp_pred_comp(cm_comp_nyr, nage); 
+  
+  for (int i=0; i<cm_comp_nyr; i++) 
+  {
+    for (int j=0;j<nage;j++)
+    {
+    tmp_pred_comp(i) = pred_cm_comp(cm_comp_yrs(i));
+    }
+  }
+  
+  for (int i=0;i<cm_comp_nyr;i++)
+  {
+    for (int j=0;j<nage;j++)
+    {
+      if (data_cm_comp(i,j) > 0)
+        //Extract years from the predicted composition matrix for which we have data
+        //int new_i = cm_comp_yrs(i);
+        Like3 += data_cm_comp(i,j)*log(tmp_pred_comp(i,j)/data_cm_comp(i,j));
+    }
+  }
+  Like3 = -1*omega_comp*Like3;
+  
+  obj_fun = dummy*dummy + Like1 + Like2 + Like3; // dummy is for testing purposes
+  
+  REPORT(Sj);
+  REPORT(Nij);
+  ADREPORT(Fi);
+  REPORT(pred_cm_cpue);
+  REPORT(pred_cm_comp);
+  REPORT(pred_catch);
+  REPORT(Like1);
+  REPORT(Like2);
+  REPORT(Like3);
+  REPORT(obj_fun);
+  
+  return(obj_fun);
+  
+  }
---
+++ tmb/mod2/mod1_datprep.R
@@ -0,0 +1,160 @@
+# Data prep for Model 1 ASA inputs
+# Author: Jane Sullivan
+# Contact: jane.sullivan1@alaska.gov
+# Last edited: 2018-04-09
+
+# Model 1 - base ASA that only includes catch, fishery CPUE, fishery
+# weight-at-age, and catch compositions. No sex structure.
+source("r/helper.r")
+source("r/functions.r")
+
+YEAR <- 2017
+
+# Harvest ----
+read_csv(paste0("data/fishery/nseiharvest_ifdb_1969_", YEAR,".csv"), 
+                       guess_max = 50000) %>% 
+  group_by(year) %>% 
+  summarize(total_pounds = sum(whole_pounds)) %>% 
+  mutate(total_kg = total_pounds * 0.453592 / 1e6) %>% 
+  select(-total_pounds) %>% 
+  filter(year >= 1980) -> sum_catch
+
+# Fishery CPUE ----
+
+# Read in data, standardize cpue, etc.
+read_csv(paste0("data/fishery/fishery_cpue_1997_", YEAR,".csv"), 
+         guess_max = 50000) %>% 
+  filter(!is.na(hook_space) & !is.na(sable_lbs_set) &
+           julian_day > 226) %>%  # if there were special projects before the fishery opened
+  mutate(# standardize hook spacing (Sigler & Lunsford 2001, CJFAS), 1 m = 39.37 in
+         std_hooks = 2.2 * no_hooks * (1 - exp(-0.57 * (hook_space / 39.37))), 
+         # convert lbs to kg
+         std_cpue_kg = sable_lbs_set * 0.453592 / std_hooks) -> fsh_cpue  
+
+# Nominal CPUE 
+fsh_cpue %>% 
+  group_by(year) %>% 
+  summarise(cpue = mean(std_cpue_kg),
+            sdev = sd(std_cpue_kg),
+            n = length(std_cpue_kg),
+            se = sdev / (n ^ (1/2)),
+            var = var(std_cpue_kg),
+            cv = sdev / cpue,
+            upper = cpue + (2 * se),
+            lower = cpue - (2 * se)) -> fsh_sum 
+
+# Historical CPUE 
+
+# From KVK: Logbooks were not included in IFDB until 1997. Commercial fishery
+# CPUE values prior to 1997, for use in the ASA or other medium, are LEGACY
+# VALUES. Jane updates: I don't have any source information from these numbers
+# other than this. Kray kept them in a csv file called
+# data/legacy_fishery_cpue.csv. Similarly, I moved and renamed the same file as
+# data/fishery/legacy_fisherycpue_1980_1996.csv
+
+read_csv("data/fishery/legacy_fisherycpue_1980_1996.csv", 
+         col_names = FALSE) %>% as.numeric() -> hist_cpue
+
+# Use the mean CV from 1997-present to estimate the variance for legacy CPUE
+# values, following KVK.
+
+data.frame(year = 1980:1996,
+           # Convert to kg
+           cpue = hist_cpue * 0.453592) %>% 
+  mutate(var = (cpue * mean(fsh_sum$cv)) ^ 2) %>% 
+  bind_rows(fsh_sum %>% 
+              select(year, cpue, var)) %>% 
+  mutate(cpue = round(cpue, 3),
+         var = round(var, 3)) -> cpue_ts
+
+axis <- tickr(cpue_ts, year, 3)
+
+ggplot(sum_catch) + 
+  geom_point(aes(year, total_kg)) +
+  geom_line(aes(year, total_kg)) +
+  geom_vline(xintercept = 1997, linetype = 2, colour = "grey") +
+  scale_x_continuous(breaks = axis$breaks, labels = axis$labels) + 
+  labs(x = "", y = "Fishery harvest\n(millions round kg)") -> catch
+
+ggplot(cpue_ts) +
+  geom_point(aes(year, cpue)) +
+  geom_line(aes(year, cpue)) +
+  geom_ribbon(aes(year, ymin = cpue - sqrt(var), ymax = cpue + sqrt(var)),
+              # geom_ribbon(aes(year, ymin = cpue - var, ymax = cpue + var),
+              alpha = 0.2,  fill = "grey") +
+  geom_vline(xintercept = 1997, linetype = 2, colour = "grey") +
+  scale_x_continuous(breaks = axis$breaks, labels = axis$labels) + 
+  lims(y = c(0, 1.4)) +
+  labs(x = "", y = "Fishery CPUE\n(round kg/hook)") -> cpue
+
+plot_grid(catch, cpue, ncol = 1, align = 'hv')
+
+merge(sum_catch, cpue_ts) %>%
+  select(year, catch = total_kg, cpue, cpue_var = var) %>% 
+  write_csv("tmb/mod1/cm_catch_cpue.csv")
+
+# Weight-at-age ----
+
+# Fishery biological data
+read_csv(paste0("data/fishery/fishery_bio_2000_", YEAR,".csv"), 
+         guess_max = 50000) %>%
+  mutate(Year = factor(year),
+         Sex = factor(Sex)) -> fsh_bio
+
+# Empirical weight-at-age 
+
+fsh_bio %>% 
+  filter(!is.na(weight) & !is.na(age) & age < 30) %>% #& !is.na(Sex)
+  group_by(age) %>% 
+  summarise(weight_kg = mean(weight) %>% round(3)) %>% 
+  bind_rows(
+    fsh_bio %>% 
+    # Plus group
+    filter(!is.na(weight) & !is.na(age) & age >= 30) %>% #& !is.na(Sex)
+      summarise(weight_kg = mean(weight) %>% round(3)) %>% 
+      mutate(age = 30)) -> emp_waa
+
+write_csv(emp_waa, "tmb/mod1/cm_waa.csv")
+
+# Age compositions ----
+
+# Combine survey and fishery data for age comp analysis
+unique(fsh_bio$year)
+fsh_bio %>% 
+  select(year, age) %>% 
+  filter(year >= 2002 & !is.na(age)) %>% 
+  droplevels() %>% 
+  mutate(age = ifelse(age >= 30, 30, age)) %>% 
+  filter(age >= 2) -> fsh_comps  # Plus group
+
+# Age comps (sex-specific)
+fsh_comps %>% 
+  count(year, age) %>%
+  group_by(year) %>% 
+  mutate(proportion = round(n / sum(n), 4)) -> agecomps   
+
+# Check that they sum to 1
+agecomps %>% 
+  group_by(year) %>% 
+  summarise(sum(proportion)) 
+
+# Sample sizes 
+agecomps %>% 
+  group_by(year) %>% 
+  summarize(n = sum(n)) 
+
+# Reshape
+agecomps %>% dcast(year ~ age, value.var = "proportion") -> agecomps
+agecomps[is.na(agecomps)] <- 0
+
+write_csv(agecomps, "tmb/mod1/cm_agecomps.csv")
+
+# Used to get logN starting values, these are the mean age comps over all years
+fsh_comps %>%
+  count(age) %>% 
+  mutate(proportion = round(n / sum(n), 4)) -> mean_comps
+
+mean_comps %>% dcast(1 ~ age, value.var = "proportion") -> meancomps
+meancomps[is.na(meancomps)] <- 0
+
+write_csv(meancomps[,-1], "tmb/mod1/meancomps.csv")
