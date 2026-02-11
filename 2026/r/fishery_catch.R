e519f6edeb5c9568a31f31a1cae1e55b0e48934f
Aaron Lambert <105386771+AaronWLambert@users.noreply.github.com>
2026-02-11 13:47:53 -0900

    Edited and ran fishery_catch.R

--- 2026/r/fishery_catch.R
+++ 2026/r/fishery_catch.R
@@ -13,10 +13,11 @@ source("r_helper/functions.r")
 # if(!require("rms"))   install.packages("rms") # simple bootstrap confidence intervals
 
 # Most recent year of data
-YEAR <- 2024
+YEAR <- 2025
 
 # Harvest ----
 
+# *** Past notes*** Aaron: may not be relevant anymore ------------------------
 # There are multiple sources of catch data. The IFDB (Region I database) was
 # used in the past, but J. Shriver recommends using the Gross Earnings File
 # (GEF) for catch histories, which go back to 1975. The problem is the GEF isn't
@@ -24,30 +25,23 @@ YEAR <- 2024
 # year's assessment. The IFDB and GEF are the same from 1985-present, so use
 # that. The SCAA model relies on Carlile et al 2002 published estimates of NSEI
 # catch 1975-1984.
+# *****************************************************************************
 
+# Load in catch data (refer to r script fishery_cpue_prep.R for details on access)
 catch_ifdb <- read_csv(paste0(YEAR+1,"/data/fishery/nseiharvest_ifdb_1985_", YEAR,".csv"), 
-                       guess_max = 50000) #%>% 
-colnames(catch_ifdb)
-#exvessel_value <- read.csv("data/exvessel_value.csv") # request from Aaron.baldwin@alaska.gov
-#update from OceanAK report for future standardization; 2022 onward will use this report...
-# link:
+                       guess_max = 50000)
 
+# Exvessel values as calculated from previous years 
 exvessel_value <- read_csv(paste0("legacy_data/exvessel_value_",
                 YEAR,".csv",sep="")) %>% 
   mutate(year=Year_Landed, exvessel_mil_usd = CFEC_Value/1000000)
 
-#exvessel_value <- read_csv(paste0(YEAR+1,"/data/exvessel_value_22ud.csv")) %>% 
-#  mutate(year=Year_Landed, exvessel_mil_usd = CFEC_Value/1000000)
-#format like Jane's old for consistency with code... just year and value
-
-View(exvessel_value)
-#Add new year from Rhea's querry: 
-#OceanAK exvessel: https://oceanak.dfg.alaska.local/analytics/saw.dll?PortalGo&Action=prompt&path=%2Fshared%2FCommercial%20Fisheries%2FRegion%20I%2FGroundFish%2FUser%20Reports%2FPhil%27s%20Sablefish%2FExvessel%20value%20estimated%20from%20fish%20ticket%20data
-# Save in YEAR/data/fishery/raw_data/Exvessel_value_for_YEAR_estimated_from_fish_ticket_data.csv
-
-exves_new <- read_csv(paste0(YEAR+1,"/data/fishery/raw_data/Exvessel value estimated from fish ticket data",YEAR,".csv")) %>% 
+#Add new year from "Phils Sablefish" querry.
+# Save in: YEAR/data/fishery/raw_data/Exvessel_value_for_YEAR_estimated_from_fish_ticket_data.csv
+exves_new <- read_csv(paste0(YEAR+1,"/data/fishery/raw_data/Exvessel_value_for_",YEAR,"_estimated_from_fish_ticket_data.csv")) %>% 
   data.frame()
 
+# Calculate and add the relevant columns data from this years exvessel data to past years
 exvessel_value[nrow(exvessel_value)+1,]<-list(YEAR,
                                               sum(exves_new$Whole.Weight..lbs.),
                                               length(unique(exves_new$CFEC)), 
@@ -62,11 +56,7 @@ exvessel_value<-exvessel_value[,c(5,6)]
 #if new year not available, add in best est. from Aaron and groundfish crew....
 #2021 exvessel value ... add in manually and
 
-view(exvessel_value)
-
-str(catch_ifdb)
-
-catch_ifdb %>% 
+catch_plot <- catch_ifdb %>% 
   filter(year > 2013) %>% 
   group_by(year, julian_day) %>% 
   dplyr::summarise(pounds = sum(whole_pounds)) %>% 
@@ -74,7 +64,7 @@ catch_ifdb %>%
   group_by(year) %>% 
   mutate(tot_pounds = sum(pounds)) %>% 
   ungroup() %>% 
-  mutate(cum_pounds = cum_pounds / tot_pounds) -> catch_plot
+  mutate(cum_pounds = cum_pounds / tot_pounds)
 
 # Cumulative catch over the fishery seasons
 ggplot(catch_plot, aes(x = julian_day, colour = factor(year), size = factor(year), group = factor(year))) +
@@ -86,22 +76,23 @@ ggplot(catch_plot, aes(x = julian_day, colour = factor(year), size = factor(year
   ylim(0, 1)
 
 # Total catch by year
-catch_ifdb %>% 
+sum_catch <- catch_ifdb %>% 
   group_by(year) %>% 
   dplyr::summarize(total_pounds = sum(whole_pounds)) %>%
   mutate(perc_ch = (total_pounds-lag(total_pounds,default = first(total_pounds)))/
-           lag(total_pounds,default = first(total_pounds))) -> sum_catch #view(sum_catch)
+           lag(total_pounds,default = first(total_pounds))) 
 
+# Get the label for %increase/decrease
 if (sum_catch$perc_ch[sum_catch$year == YEAR] > 0) {
   pchange<-paste0(round(sum_catch$perc_ch[sum_catch$year == YEAR]*100,1),"% increase")
 } else {
   pchange<-paste0(round(sum_catch$perc_ch[sum_catch$year == YEAR]*100,1),"% decrease")
 }
 
-# axis <- tickr(sum_catch, year, 5)
-ggplot(sum_catch %>% 
-         filter(year >= 1985), 
-       aes(x = year, y = total_pounds/1e6)) +
+# Plot the catch in millions rd lbs over time
+catch <- ggplot(sum_catch %>% 
+                  filter(year >= 1985), 
+                aes(x = year, y = total_pounds/1e6)) +
   geom_line(group=1) +
   geom_point() +
   # scale_x_continuous(breaks = axis$breaks, labels = axis$labels) + 
@@ -112,21 +103,26 @@ ggplot(sum_catch %>%
   labs(x = NULL, y = "Catch (million round lb)\n") +
   annotate("text", x = 2015, y = 5, 
            label = pchange,
-           size=8)-> catch
+           size=8)
 
+catch
+
+# Save the total catch by year
 write_csv(sum_catch, paste0(YEAR+1,"/output/harvest_1985_", YEAR, ".csv"))
 
 catch
 ggsave(paste0(YEAR+1,"/figures/fishery_harvest_1985_", YEAR, ".png"), 
        dpi=300,  height=4, width=7,  units="in")
 
-# Catch by ports
-'%ni%' <- Negate('%in%')
-
+# Look at distinct ports and vessels....
 catch_ifdb %>% filter(year == YEAR) %>% distinct(Port)
 catch_ifdb %>% filter(year == YEAR) %>% distinct(Vessel)
-# catch_ifdb %>% filter(year == YEAR & Port == "HOM") 
-catch_ifdb %>% 
+
+# Create function to look whats "not in" 
+'%ni%' <- Negate('%in%')
+
+# Get port catch
+port_catch <- catch_ifdb %>% 
   mutate(Port = derivedFactor(`SIT` = Port == "SIT",
                               `JNU` = Port == "JNU",
                               `PBG` = Port == "PBG",
@@ -139,12 +135,14 @@ catch_ifdb %>%
   filter(n_vessels > 3) %>% 
   group_by(year) %>% 
   mutate(tot_pounds = sum(pounds),
-         perc = pounds/tot_pounds * 100) -> port_catch
+         perc = pounds/tot_pounds * 100)
 
+# Catch by port
 ggplot(port_catch, aes(x = year, y = pounds/1e6, colour = Port, group = Port)) +
   geom_line()
 
-ggplot(port_catch, aes(x = year, y = perc, fill = Port)) +
+# Percent catch by port
+port <- ggplot(port_catch, aes(x = year, y = perc, fill = Port)) +
   geom_bar(stat = "identity", width = 1, colour = "black") +
   scale_fill_grey(start = 0.15, end = 1) +
   scale_x_continuous(breaks = seq(1985,2022,5), labels = seq(1985,2022,5)) +
@@ -155,14 +153,16 @@ ggplot(port_catch, aes(x = year, y = perc, fill = Port)) +
         legend.background = element_rect(color = "black", 
                                          fill = "white", 
                                          linetype = "solid")) +
-  labs(x = NULL, y = "Percent landings by port", fill = NULL) -> port 
+  labs(x = NULL, y = "Percent landings by port", fill = NULL)
 
+# Put into one figure for reporting
 plot_grid(catch, port, ncol = 1, align = 'hv')
 
 ggsave(paste0(YEAR+1,"/figures/catch_byport_", YEAR, ".png"),
        dpi=300, height=10, width=7, units="in")
 
 # Exvessel value ----
+# First, get label for exvessel plot that tells the increase/decrease
 exvessel_value<-exvessel_value %>% 
   mutate(perc_ch = (exvessel_mil_usd-lag(exvessel_mil_usd,default = first(exvessel_mil_usd)))/
          lag(exvessel_mil_usd,default = first(exvessel_mil_usd))) #view(sum_catch)
@@ -173,6 +173,7 @@ if (exvessel_value$perc_ch[exvessel_value$year == YEAR] > 0) {
   pchange_evv<-paste0(round(exvessel_value$perc_ch[exvessel_value$year == YEAR]*100,1),"% decrease")
 }
 
+#Plot of exvessel value over time
 exvessel <- ggplot(exvessel_value, aes(x = year, y = exvessel_mil_usd)) +
   #exvessel <- ggplot(exvessel_value, aes(x = Year.Landed, y = CFEC.Value)) +
   geom_point() +
@@ -186,21 +187,25 @@ exvessel <- ggplot(exvessel_value, aes(x = year, y = exvessel_mil_usd)) +
   annotate("text", x = YEAR-7, y = 10, 
            label = pchange_evv,
            size=8)
+
+# Save th plot
 exvessel
 ggsave(paste0(YEAR+1,"/figures/exvessel_value_1985_", YEAR, ".png"), 
        dpi=300,  height=4, width=7,  units="in")
 
+
+# Three panel fig for report
 plot_grid(catch, port, exvessel, ncol = 1)#, align = 'hv') 
 
 ggsave(paste0(YEAR+1,"/figures/catch_exvesselvalue_", YEAR, "v3.png"),
        dpi=300, height=10, width=7, units="in")
-# View(port_catch)
+
+
 
 data.frame(exvessel_value)
 
-# Catch by gear: 
-unique(catch_ifdb$Gear)
-catch_ifdb %>% 
+# Catch by gear
+gear_catch <- catch_ifdb %>% 
   mutate(Gear = ifelse(is.na(Gear),"Longline",Gear)) %>% 
   filter(year >= 1985) %>% 
   group_by(year, Gear) %>% 
@@ -210,13 +215,15 @@ catch_ifdb %>%
   filter(n_vessels > 3) %>% 
   group_by(year) %>% 
   mutate(tot_pounds = sum(pounds),
-         perc = pounds/tot_pounds * 100) -> gear_catch
+         perc = pounds/tot_pounds * 100) 
 
+# Catch by gear (I,e. pot or longline hook and line)
 ggplot(gear_catch, aes(x = year, y = pounds/1e6, colour = Gear, group = Gear)) +
   geom_line() + geom_point()
 
-# This plot is lame but will be cooler as the pot fishery develops
-ggplot(gear_catch, aes(x = year, y = perc, fill = Gear)) +
+# This plot is lame but will be cooler as the pot fishery develops...
+# Plot proportion of gear type by year
+gear <- ggplot(gear_catch, aes(x = year, y = perc, fill = Gear)) +
   geom_bar(stat = "identity", width = 1, colour = "black") +
   scale_fill_grey(start = 0.15, end = 1) +
   scale_x_continuous(breaks = seq(1985,2022,5), labels = seq(1985,2022,5)) +
@@ -227,8 +234,9 @@ ggplot(gear_catch, aes(x = year, y = perc, fill = Gear)) +
         legend.background = element_rect(color = "black", 
                                          fill = "white", 
                                          linetype = "solid")) +
-  labs(x = NULL, y = "Percent landings by port", fill = NULL) -> gear 
+  labs(x = NULL, y = "Percent Gear Type", fill = NULL) 
 
+# Plot catch by gear over time
 ggplot(sum_catch %>% 
          filter(year >= 1985), 
        aes(x = year, y = total_pounds/1e6)) +
@@ -248,16 +256,19 @@ ggplot(sum_catch %>%
            label = pchange,
            size=8)-> catch_bygear
 
-myColors <- c("blue","black","darkcyan")
+# Aarons favorite colorblind friendly pallette
+wong <- c("#000000", "#E69F00", "#56B4E9", "#009E73", 
+          "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-ggplot(rbind(sum_catch %>% mutate(Gear = "Longline & Pot") %>% 
-               select(-perc_ch) %>% filter(year >= 2021),
-             gear_catch %>% #filter(year >= 2021) %>% 
-               select(year,total_pounds = pounds, Gear)), 
-       aes(x = year, y = total_pounds/1e6, col = Gear, group = Gear, fill = Gear)) +
+# Cactch by gear
+catch_bygear <- ggplot(rbind(sum_catch %>% mutate(Gear = "Longline & Pot") %>% 
+                               select(-perc_ch) %>% filter(year >= 2021),
+                             gear_catch %>% #filter(year >= 2021) %>% 
+                               select(year,total_pounds = pounds, Gear)), 
+                       aes(x = year, y = total_pounds/1e6, col = Gear, group = Gear, fill = Gear)) +
   geom_line() +
   geom_point(aes(col = Gear)) +
-  scale_colour_manual(values = myColors) +
+  scale_colour_manual(values = wong) +
   # scale_x_continuous(breaks = axis$breaks, labels = axis$labels) + 
   scale_x_continuous(breaks = seq(1985,2022,5), labels = seq(1985,2022,5)) + 
   scale_y_continuous(breaks = seq(0, 6, 1), limits = c(0, 6), labels = seq(0, 6, 1)) +
@@ -267,7 +278,8 @@ ggplot(rbind(sum_catch %>% mutate(Gear = "Longline & Pot") %>%
   annotate("text", x = 2015, y = 5, 
            label = pchange,
            size=8) +
-  theme(legend.position = c(.2, .2)) -> catch_bygear
+  theme(legend.position = c(.2, .2)) 
+
 
 plot_grid(catch_bygear, port, exvessel,  ncol = 1, align = 'hv')
 
@@ -300,6 +312,3 @@ full_join(sum_catch,exvessel_value,by="year") %>%
   scale_colour_manual(values = c("red", "black"), guide = FALSE) +
   labs(x = "\nCatch (million round lb)", y = "Ex-vessel value (million USD)") +
   scale_y_continuous(limits = c(0,6))
-
-#------------------------------------------------------------------------------
-#_______________________________________________________________________________
